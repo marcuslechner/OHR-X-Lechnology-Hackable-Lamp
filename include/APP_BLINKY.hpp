b78c8fe6fc66d510847169a59c03380bcdbc5634
marcuslechner <lechnologyprojects@gmail.com>
2025-05-03 19:36:27 -0400

    set up timer and blinky

---
+++ include/APP_BLINKY.hpp
@@ -0,0 +1,12 @@
+#ifndef APP_BLINKY_HPP
+#define APP_BLINKY_HPP
+
+
+namespace APP_BLINKY
+{
+    void init();
+    void process();
+}
+
+
+#endif // APP_BLINKY_HPP
---
+++ src/APP_TIMER.cpp
@@ -0,0 +1,49 @@
+#include "APP_TIMER.hpp"
+#include <Arduino.h>
+
+Timer::Timer(unsigned long intervalMs, bool startNow)
+    : _interval(intervalMs),
+      _lastTime(startNow ? millis() : 0),
+      _enabled(startNow)
+{}
+
+void Timer::start()
+{
+    _lastTime = millis();
+    _enabled = true;
+}
+
+void Timer::stop()
+{
+    _enabled = false;
+}
+
+void Timer::reset()
+{
+    _lastTime = millis();
+}
+
+bool Timer::expired()
+{
+    if (!_enabled)
+        return false;
+
+    unsigned long now = millis();
+    if (now - _lastTime >= _interval)
+    {
+        _lastTime = now;
+        return true;
+    }
+
+    return false;
+}
+
+void Timer::setInterval(unsigned long intervalMs)
+{
+    _interval = intervalMs;
+}
+
+bool Timer::isRunning() const
+{
+    return _enabled;
+}
