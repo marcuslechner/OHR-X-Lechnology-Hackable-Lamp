65a51e6bca121afd466cebae59a34dfa2cf1c065
marcuslechner <lechnologyprojects@gmail.com>
2025-05-06 23:58:05 -0400

    Servo motor works fully and added test case to verify

--- include/APP_SERVO.hpp
+++ include/APP_SERVO.hpp
@@ -9,7 +9,7 @@
 #ifndef APP_SERVO_HPP
 #define APP_SERVO_HPP
 
-namespace APP_SERVO //namespace named APP_SERVO, allows unambiguous calls of begin and update, can have multiple function of init() accross multiple header files
+namespace APP_SERVO //public namespace named APP_SERVO, allows unambiguous calls of begin and update, can have multiple function of init() accross multiple header files
 { //alternative to a name space would be APP_SERVO_init(), instead we call the namespace function APP_SERVO::init()
     //adds structure to the state machine
     void init();
